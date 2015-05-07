afa8e85f15e6897e8483d2128302d6e5ecc35371
Matthew Ankerson <sm.ankerson@gmail.com>
2015-05-08 11:22:45 +1200

    scrapped project, created new with proper bootstrap and copied files over

--- ABLCloudStaff/ABLCloudStaff/Global.asax.cs
+++ ABLCloudStaff/ABLCloudStaff/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Data.Entity;
 using ABLCloudStaff.Models;
@@ -14,7 +15,9 @@ namespace ABLCloudStaff
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             // Initialise the database (for testing)
             Database.SetInitializer(new DropCreateABLCloudStaffAlways());
