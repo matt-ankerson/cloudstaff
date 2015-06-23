691f54bb96c1b528312b88967f5e9ddc2e3f7cef
Matthew Ankerson <sm.ankerson@gmail.com>
2015-06-23 13:37:38 +1200

    wiped deans repo again and placed all project files back in

---
+++ ABLCloudStaff/ABLCloudStaff/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+using ABLCloudStaff.Models;
+
+namespace ABLCloudStaff
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialise the database (for testing)
+            Database.SetInitializer(new DropCreateABLCloudStaffAlways());
+        }
+    }
+}
