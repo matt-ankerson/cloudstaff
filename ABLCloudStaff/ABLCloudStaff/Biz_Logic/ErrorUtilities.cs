6514c7e90817f36ff43f0b7d777ac4e432d7c885
Matthew Ankerson <sm.ankerson@gmail.com>
2015-09-25 15:40:15 +1200

    added error class in the model and function in error utilities to store error information

---
+++ ABLCloudStaff/ABLCloudStaff/Biz_Logic/ErrorUtilities.cs
@@ -0,0 +1,36 @@
+﻿// Author: Matt Ankerson
+// Date: 25 September
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ABLCloudStaff.Models;
+
+namespace ABLCloudStaff.Biz_Logic
+{
+    /// <summary>
+    /// Provide a means of logging errors thrown within the app to the database table 'Errors'.
+    /// </summary>
+    public class ErrorUtilities
+    {
+        /// <summary>
+        /// Log given exception details in the Error table
+        /// </summary>
+        /// <param name="exception">The main exception text</param>
+        /// <param name="timeStamp">The time the error occured</param>
+        /// <param name="innerException">Optional field for inner exception.</param>
+        /// <param name="detail">Optional further detail about the nature of the exception.</param>
+        public static void LogException(string exception, DateTime timeStamp, string innerException=null, string detail=null)
+        {
+            using (var context = new ABLCloudStaffContext())
+            {
+                // Craft the error instance.
+                Error err = new Error { Exception = exception, TimeStamp = timeStamp, InnerException = innerException, Detail = detail };
+                // Save to error table.
+                context.Errors.Add(err);
+                context.SaveChanges();
+            }
+        }
+    }
+}
