9ed25f2636765222e575da093c9641d74ab2ee7d
Matthew Ankerson <sm.ankerson@gmail.com>
2015-10-06 11:12:16 +1300

    added small display view for sharepoint at Marks request

--- ABLCloudStaff/ABLCloudStaff/Controllers/DisplaySmallController.cs
+++ ABLCloudStaff/ABLCloudStaff/Controllers/DisplaySmallController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ABLCloudStaff.Models;
+using ABLCloudStaff.Biz_Logic;
 
 namespace ABLCloudStaff.Controllers
 {
     public class DisplaySmallController : Controller
     {
-        //
-        // GET: /DisplaySmall/
+        /// <summary>
+        /// Get all core instances, push to the display small view.
+        /// </summary>
+        /// <returns>Action result object</returns>
         public ActionResult DisplaySmall()
         {
-            return View();
+            List<Core> coreInfo = CoreUtilities.GetAllCoreInstances();
+
+            return View(coreInfo);
         }
 	}
 }
