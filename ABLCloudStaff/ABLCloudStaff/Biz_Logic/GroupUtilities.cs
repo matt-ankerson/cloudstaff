70cb5df2fe024716121eddce1804558a910e844f
Matthew Ankerson <sm.ankerson@gmail.com>
2015-10-15 15:39:30 +1300

    logic to access and remove groups in biz logic layer

---
+++ ABLCloudStaff/ABLCloudStaff/Biz_Logic/GroupUtilities.cs
@@ -0,0 +1,188 @@
+﻿// Author: Matt Ankerson
+// Date: 15 October 2015
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ABLCloudStaff.Models;
+
+namespace ABLCloudStaff.Biz_Logic
+{
+    /// <summary>
+    /// Provides utilities for groups.
+    /// </summary>
+    public static class GroupUtilities
+    {
+        /// <summary>
+        /// Add a group to the database with 'Now' for the timestamp
+        /// </summary>
+        /// <param name="userIDs">List of UserIDs to include in this group.</param>
+        public static void AddGroup(List<int> userIDs)
+        {
+            try
+            {
+                // Serialise the list of userIDs to a comma separated string representation.
+                string members = "";
+
+                foreach (int userID in userIDs)
+                {
+                    members += userID.ToString() + ",";
+                }
+
+                using (var context = new ABLCloudStaffContext())
+                {
+                    // Create new group
+                    Group grp = new Group { Members = members, Active = true, Initiated = DateTime.Now };
+                    // Add and save.
+                    context.Groups.Add(grp);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorUtilities.LogException(ex.Message, DateTime.Now);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Deactivate the indicated group.
+        /// </summary>
+        /// <param name="groupID">The group to deactivate.</param>
+        public static void RemoveGroup(int groupID)
+        {
+            try
+            {
+                using (var context = new ABLCloudStaffContext())
+                {
+                    // Pull up the indicated Group instance
+                    Group grp = context.Groups.Where(x => x.GroupID == groupID).FirstOrDefault();
+                    // Deactivate the group.
+                    grp.Active = false;
+                    // Save changes
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorUtilities.LogException(ex.Message, DateTime.Now);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Get all currently actve groups.
+        /// </summary>
+        /// <returns>List of active groups, with member lists deserialised.</returns>
+        public static List<GroupDeserialized> GetActiveGroups()
+        {
+            List<GroupDeserialized> groups = new List<GroupDeserialized>();
+
+            try
+            {
+                List<Group> rawGroups = new List<Group>();
+
+                using (var context = new ABLCloudStaffContext())
+                {
+                    // Get all active groups from database.
+                    rawGroups = context.Groups.Where(x => x.Active == true).OrderByDescending(x => x.Initiated).ToList();
+                }
+
+                // Build list of deserialized groups.
+                foreach (var rawGroup in rawGroups)
+                {
+                    var stringMembers = rawGroup.Members.Split(',');
+                    List<int> intMembers = new List<int>();
+
+                    // Loop over string array of members:
+                    foreach (string stringMember in stringMembers)
+                    {
+                        intMembers.Add(Convert.ToInt32(stringMember));
+                    }
+
+                    // Create deserialised group object
+                    GroupDeserialized grpDsrlzd = new GroupDeserialized { 
+                        Members = intMembers, 
+                        Active = rawGroup.Active, 
+                        GroupID = rawGroup.GroupID, 
+                        Initiated = rawGroup.Initiated 
+                    };
+
+                    // Add object to list
+                    groups.Add(grpDsrlzd);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorUtilities.LogException(ex.Message, DateTime.Now);
+                throw ex;
+            }
+
+            return groups;
+        }
+
+        /// <summary>
+        /// Get all groups regardless of whether or not they're actve.
+        /// </summary>
+        /// <returns>List of groups with deserialized members.</returns>
+        public static List<GroupDeserialized> GetAllGroups()
+        {
+            List<GroupDeserialized> groups = new List<GroupDeserialized>();
+
+            try
+            {
+                List<Group> rawGroups = new List<Group>();
+
+                using (var context = new ABLCloudStaffContext())
+                {
+                    // Get all active groups from database.
+                    rawGroups = context.Groups.OrderByDescending(x => x.Initiated).ToList();
+                }
+
+                // Build list of deserialized groups.
+                foreach (var rawGroup in rawGroups)
+                {
+                    var stringMembers = rawGroup.Members.Split(',');
+                    List<int> intMembers = new List<int>();
+
+                    // Loop over string array of members:
+                    foreach (string stringMember in stringMembers)
+                    {
+                        intMembers.Add(Convert.ToInt32(stringMember));
+                    }
+
+                    // Create deserialised group object
+                    GroupDeserialized grpDsrlzd = new GroupDeserialized
+                    {
+                        Members = intMembers,
+                        Active = rawGroup.Active,
+                        GroupID = rawGroup.GroupID,
+                        Initiated = rawGroup.Initiated
+                    };
+
+                    // Add object to list
+                    groups.Add(grpDsrlzd);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorUtilities.LogException(ex.Message, DateTime.Now);
+                throw ex;
+            }
+
+            return groups;
+        }
+    }
+
+    /// <summary>
+    /// Deserialised version of the group class. (list of integers instead of comma separated string)
+    /// </summary>
+    public class GroupDeserialized
+    {
+        public int GroupID { get; set; }
+        public List<int> Members { get; set; }
+        public DateTime Initiated { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ ABLCloudStaff/ABLCloudStaff/Models/Group.cs
@@ -0,0 +1,23 @@
+﻿// Author: Matt Ankerson
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABLCloudStaff.Models
+{
+    /// <summary>
+    /// Class intended to maintain groups of people that leave the office
+    /// </summary>
+    /// <remarks>
+    /// It was decided that this class would be quite minimal. For the collection of historical data, we're relying on the records
+    /// created by changing the state of each individual user.
+    /// </remarks>
+    public class Group
+    {
+        public int GroupID { get; set; }
+        public string Members { get; set; }         // Serialised collection of (comma delimited) integers representing members by UserID.
+        public DateTime Initiated { get; set; }     // Time that this group was initiated.
+        public bool Active { get; set; }            // Whether or not this group has returned or not.
+    }
+}
